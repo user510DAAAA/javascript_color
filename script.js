f15f98e85602db3cfe6489828739c86595c6e330
user510DAAAA <131327154+user510DAAAA@users.noreply.github.com>
2024-09-24 10:43:02 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// JavaScript
+
+//console.log('Hello world!');
+//console.log(document.getElementById('myColor').value);
+//document.getElementById('bodyText').textContent = document.getElementById('myColor').value;
+//定数の宣言
+const color = document.getElementById('myColor');
+const text = document.getElementById('bodyText');
+text.textContent = color.value;
+function changeColor(){
+    // カラーコードを表示
+    document.body.style.backgroundColor = color.value;
+    if (color.value === '#ffffff') {
+        text.textContent = 'コード：' + color.value + '(white)';
+      } else if (color.value === '#000000') {
+        text.textContent = 'コード：' + color.value + '(black)';
+      } else {
+        text.textContent = 'コード：' + color.value;
+      }
+    }
+      // 背景色を変更
+  
+    
+// カラーピッカーで色が選択されたら、changeColorを動作させる
+color.addEventListener('input', changeColor);
+
